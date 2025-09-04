d83d0a8d5844ab89d86a9fc9276313c3e127eac2
sachin <sachin@geekonomy.in>
2025-09-04 14:41:43 +0530

    Refactor email sending functionality to use Gmail service for password reset emails

--- src/utils/sendEmails.js
+++ src/utils/sendEmails.js
@@ -1,30 +1,26 @@
 import nodemailer from 'nodemailer';
 
+let transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "t85794920@gmail.com",   
+    pass: "ojmlesolysmslpxx"      
+  },
+});
+
 export async function sendResetEmail(toEmail, resetLink) {
-  let testAccount = await nodemailer.createTestAccount();
-
-  let transporter = nodemailer.createTransport({
-    host: "smtp.ethereal.email",
-    port: 587,
-    auth: {
-      user: testAccount.user,
-      pass: testAccount.pass,
-    },
-  });
-
   let info = await transporter.sendMail({
-    from: '"MyApp Support" <no-reply@myapp.com>',
+    from: '"MyApp Support" <yourgmail@gmail.com>',
     to: toEmail,
     subject: "Password Reset Request",
     html: `
       <h2>Password Reset</h2>
-      <p>Click the link below to reset your password:</p>
+      <p>Click below to reset your password:</p>
       <a href="${resetLink}" target="_blank">${resetLink}</a>
-      <p>This link will expire in 15 minutes.</p>
     `,
   });
 
-  console.log("Preview Email URL: %s", nodemailer.getTestMessageUrl(info));
+  console.log("Message sent:", info.messageId);
 }
 
 
